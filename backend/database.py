393836adbbdcd803f53e0be251c83af58c6ae0bb
Anubhav Shukla <anubhavshukla1523@gmail.com>
2026-07-12 18:34:36 +0530

    Remove database password

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL ="mysql+pymysql://root:renu%401976@localhost/business_dashboard"
+DATABASE_URL = "mysql+pymysql://root:YOUR_PASSWORD@localhost/business_dashboard"
 
 engine = create_engine(DATABASE_URL)
 
